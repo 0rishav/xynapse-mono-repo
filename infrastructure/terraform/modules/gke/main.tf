912bf1b8a7a434e3e06f4dc7c62972e562e4ae0c
Rishav Raj <96721375+0rishav@users.noreply.github.com>
2026-05-10 17:33:44 +0530

    Stage (#5)
    * infra pipeline
    
    * infra pipeline
    
    * infra pipeline
    
    * infra pipeline
    
    * infra pipeline
    
    * infra pipeline
    
    * infra pipeline
    
    * infra pipeline
    
    * infra pipeline
    
    * microservice pipeline test

--- infrastructure/terraform/modules/gke/main.tf
+++ infrastructure/terraform/modules/gke/main.tf
@@ -15,7 +15,7 @@ resource "google_container_cluster" "primary" {
     master_ipv4_cidr_block  = "172.16.0.0/28"
   }
 
-  # Enabling Alias IPs (Mapping to the VPC secondary ranges)
+  # Enabling Alias IPs 
   ip_allocation_policy {
     cluster_secondary_range_name  = "gke-pods"
     services_secondary_range_name = "gke-services"
@@ -45,7 +45,6 @@ resource "google_container_node_pool" "primary_nodes" {
 
     disk_size_gb = 20                
     disk_type    = "pd-standard"
-    
     tags = ["gke-node"]
 
     labels = {

6aa0c53ff4dacec497450b11cb0f2dc2018f2d9c
Rishav Raj <rishavrajraj1155@gmail.com>
2026-05-10 13:37:01 +0000

    identity-test

--- apps/identity-service/src/index.js
+++ apps/identity-service/src/index.js
@@ -31,6 +31,8 @@ const ENVIRONMENT = process.env.NODE_ENV;
 console.log(ENVIRONMENT)
 console.log(ENVIRONMENT)
 console.log(ENVIRONMENT)
+console.log(ENVIRONMENT)
+
 
 
 const startServer = async () => {

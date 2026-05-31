8932a35582a73d9ccda725d1cc8a9a4986c51a21
Rishav Raj <rishavrajraj1155@gmail.com>
2026-05-31 07:44:13 +0000

    testing pipeline

--- packages/common/src/middleware/auth.js
+++ packages/common/src/middleware/auth.js
@@ -22,7 +22,7 @@ export const isAuthenticated = CatchAsyncError(async (req, res, next) => {
     console.log(decodedToken)
     console.log(decodedToken)
     console.log(decodedToken)
-
+    console.log(decodedToken)
 
     req.user = {
       _id: decodedToken._id,

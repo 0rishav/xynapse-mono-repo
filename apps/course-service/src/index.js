25e69b7b18f41fa33b0bdbd9b4b90d449215291b
Rishav Raj <rishavrajraj1155@gmail.com>
2026-05-31 13:56:16 +0000

    pipeline test

--- apps/course-service/src/index.js
+++ apps/course-service/src/index.js
@@ -36,6 +36,8 @@ const rootEnvPath = path.resolve(__dirname, "../../../.env");
 dotenv.config({ path: rootEnvPath });
 
 console.log("App starting...");
+console.log("App starting...");
+
 
 const app = express();
 const server = http.createServer(app);

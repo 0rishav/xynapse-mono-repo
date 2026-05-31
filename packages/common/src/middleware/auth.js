d942cf3692fe65b2582cfdcdf0ef1b18a251bb81
Rishav Raj <rishavrajraj1155@gmail.com>
2026-05-31 11:53:25 +0000

    pipeline test

--- packages/common/src/middleware/auth.js
+++ packages/common/src/middleware/auth.js
@@ -20,9 +20,6 @@ export const isAuthenticated = CatchAsyncError(async (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     console.log(decodedToken)
-    console.log(decodedToken)
-    console.log(decodedToken)
-    console.log(decodedToken)
 
     req.user = {
       _id: decodedToken._id,
